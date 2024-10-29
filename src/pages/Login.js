117ba83ac3f24934bbb4f33e5faa00fb7e6de13d
Raymond Mendoza <raymondoza28@gmail.com>
2024-10-29 02:04:50 -0500

    Skeleton for Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,23 @@
-const Login = () => {
-    return <h1>Login page</h1>
+import React from 'react';
+import './Login.css';
+
+const Contact = () => {
+    return (
+        <section className='contact'>
+            <form>
+                <h2>Sign In</h2>
+                <div className='input-box'>
+                    <label>Full Name</label>
+                    <input type='text' name='name' className='field' placeholder='Enter your name' required/>
+                </div>
+                <div className='input-box'>
+                    <label>Email Address</label>
+                    <input type='email' name='email' className='field' placeholder='Enter your email' required/>
+                </div>
+                <button type='submit'>Send Message</button>
+            </form>
+        </section>
+    )
 }
-export default Login;
+
+export default Contact;
